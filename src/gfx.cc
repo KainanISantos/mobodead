182f433d0f6f34c03576847515815bb68b2819a2
Joao Pedro Hrihorowitsch Silva <joao.h.silva@protonmail.com>
2019-10-16 21:36:32 -0300

    added renderer interface and a strange implementation for it :s

--- src/gfx.cc
+++ src/gfx.cc
@@ -1,7 +1,19 @@
 #include "gfx.hh"
 #include <GL/glew.h>
 
+#include <vector>
 #include <iostream>
+#include <stack>
+
+namespace gfx {
+	std::vector<renderer*> renderers;
+	std::stack<renderer_factory*> factories;
+
+	void add_renderer_factory(renderer_factory* f)
+	{
+		factories.push(f);
+	}
+}
 
 void gfx::init()
 {
@@ -12,10 +24,21 @@ void gfx::init()
 		std::cout << "GLEW error: " << glew_error << std::endl;
 		std::exit(-1);
 	}
+
+	while(!factories.empty())
+	{
+		renderers.push_back(factories.top()->create_renderer());
+		delete factories.top();
+		factories.pop();
+	}
 }
 
 void gfx::render()
-{
+{	
+	for(renderer* rend : renderers)
+	{
+		rend->render();
+	}
 }
 
 void gfx::end()
--- src/gfx.hh
+++ src/gfx.hh
@@ -1,7 +1,39 @@
 #ifndef GFX_HH
 #define GFX_HH
 
+#include "gfx_gl.hh"
+#include <functional>
+
 namespace gfx { 
+	class renderer
+	{
+	public:
+		virtual void render() = 0;
+		virtual void* create() = 0;
+		virtual void  destroy(void* object) = 0;
+	};
+	
+	class renderer_factory
+	{
+	public:
+		virtual renderer* create_renderer() = 0;
+	};
+	
+	template<class T>
+	void register_renderer()
+	{
+		class renderer_factory_impl : public renderer_factory
+		{
+			renderer* create_renderer() override
+			{
+				return new T;
+			}
+		};
+		extern void add_renderer_factory(renderer_factory* factory);
+		
+		add_renderer_factory(new renderer_factory_impl);
+	}
+
 	void init();
 	void render();
 	void end();
