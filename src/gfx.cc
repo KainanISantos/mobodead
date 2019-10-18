be8c48138cbcb254bbfeaa120e71ba43d1d14c63
Joao Pedro Hrihorowitsch Silva <joao.h.silva@protonmail.com>
2019-10-17 21:46:28 -0300

    Revert the renderer part. I need to think more.

--- src/gfx.cc
+++ src/gfx.cc
@@ -1,19 +1,7 @@
 #include "gfx.hh"
 #include <GL/glew.h>
 
-#include <vector>
 #include <iostream>
-#include <stack>
-
-namespace gfx {
-	std::vector<renderer*> renderers;
-	std::stack<renderer_factory*> factories;
-
-	void add_renderer_factory(renderer_factory* f)
-	{
-		factories.push(f);
-	}
-}
 
 void gfx::init()
 {
@@ -24,27 +12,12 @@ void gfx::init()
 		std::cout << "GLEW error: " << glew_error << std::endl;
 		std::exit(-1);
 	}
-
-	while(!factories.empty())
-	{
-		renderers.push_back(factories.top()->create_renderer());
-		delete factories.top();
-		factories.pop();
-	}
 }
 
 void gfx::render()
-{	
-	for(renderer* rend : renderers)
-	{
-		rend->render();
-	}
+{
 }
 
 void gfx::end()
 {
-	for(renderer* rend : renderers)
-	{
-		delete rend;
-	}
 }
